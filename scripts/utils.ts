3dc57045fbed9b996e580b83b4e64790c6fc0a46
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-08-04 22:32:26 +0300

    lint: global require

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,5 +1,6 @@
 import { resolve } from 'node:path'
 import { bgCyan, black } from 'kolorist'
+require('node:process')
 
 export const port = parseInt(process.env.PORT || '') || 3303
 export const r = (...args: string[]) => resolve(__dirname, '..', ...args)
